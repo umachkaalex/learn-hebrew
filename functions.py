ddeedd65f4fea66dc7dafc60c15fe00a86addc62
umachkaalex <42827893+umachkaalex@users.noreply.github.com>
2019-06-01 23:43:25 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -192,7 +192,7 @@ def noun_input(conn_obj, table_name='noun', lang='RUS'):
 def add_words_to_dict(conn_dict, table_name, conn_obj):  
   add_next_row = True
   while add_next_row:
-    add_next_row = create_and_save_input(table_name, conn_obj)  
+    add_next_row = noun_input(conn_obj, table_name=table_name, lang='RUS') 
     table = read_table(table_name, conn_obj)
     print(table.tail(3))
     time.sleep(3)    
