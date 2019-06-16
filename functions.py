2a55271fba89a893a68c073fc5a8cf17eadbd290
umachkaalex <42827893+umachkaalex@users.noreply.github.com>
2019-06-16 07:41:37 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -236,7 +236,7 @@ def check_nouns(conn_obj, table='noun'):
     
     input_word = input(transl_phrase) or ''  
     if input_word != '.':
-      while input_word == '\':
+      while input_word == '/':
         print(h_word[:help])
         help +=1
         input_word = input(transl_phrase) or ''  
